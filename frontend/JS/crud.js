3a7d9fb837d4d8647183c5210e937c5159139c04
MauroCD5 <61813030+MauroCD5@users.noreply.github.com>
2020-11-16 16:32:32 -0600

    a

--- frontend/JS/crud.js
+++ frontend/JS/crud.js
@@ -90,7 +90,7 @@ function updateRecord(formData) {
 }
 
 function onDelete(td) {
-    if (confirm('Are you sure to delete this record ?')) {
+    if (confirm('¿Deseas borrar este producto?')) {
         row = td.parentElement.parentElement;
         document.getElementById("employeeList").deleteRow(row.rowIndex);
         resetForm();
