4cd577de30b3afde5e8968f0fa9c383a51f3e83f
MauroCD5 <61813030+MauroCD5@users.noreply.github.com>
2020-11-24 22:08:51 -0600

    validaciones

--- frontend/JS/login.js
+++ frontend/JS/login.js
@@ -1,12 +1,63 @@
 var nombre = document.getElementById('nombre');
-var apellido = document.getElementById('apellido');
+var apellidos = document.getElementById('apellidos');
 var telefono = document.getElementById('telefono');
 var email = document.getElementById('email');
-var fechaNacimiento = document.getElementById('fechaNacimiento'),
+var fechaNacimiento = document.getElementById('fechaNacimiento');
+var contrasena = document.getElementById('contrasena');
+var error = document.getElementById('error');
 
+error.style.color = 'red';
+error.style.marginLeft = '15px';
+error.style.fontSize = '20px';
 
-    function enviarDatos() {
-        console.log('Enviando datos del usuario');
+function enviarDatos() {
+    console.log('Enviando datos del usuario');
 
-        return false;
+    var errores = [];
+
+    if (nombre.value === null || nombre.value === '') {
+
+        errores.push('Ingresa tu nombre');
     }
+
+    if (nombre.value.length > 10) {
+
+        errores.push('Ingresa un Nombre con un maximo de 50 caracteres');
+    }
+
+    if (apellidos.value === null || apellidos.value === '') {
+
+        errores.push('Ingresa tus apellidos');
+    }
+
+    if (telefono.value === null || telefono.value === '') {
+
+        errores.push('Ingresa tu telefono');
+    }
+
+    if (telefono.value.length > 10 || telefono.value.length < 10) {
+
+        errores.push('El numero de telefono debe contener 10 digitos');
+    }
+
+
+    if (email.value === null || email.value === '') {
+
+        errores.push('Ingresa tu email');
+    }
+
+    if (fechaNacimiento.value === null || fechaNacimiento.value === '') {
+
+        errores.push('Ingresa tu fecha de nacimiento');
+    }
+
+    if (contrasena.value === null || contrasena.value === '') {
+
+        errores.push('Ingresa tu contrasena');
+    }
+
+    error.innerHTML = errores.join(', ');
+
+
+    return false;
+}
