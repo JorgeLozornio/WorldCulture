855f95ba6024fcac027855aa075c0ed450421798
MauroCD5 <61813030+MauroCD5@users.noreply.github.com>
2020-11-24 21:15:14 -0600

    validaciones

--- frontend/JS/login.js
+++ frontend/JS/login.js
@@ -1,1 +1,12 @@
-var selec
+var nombre = document.getElementById('nombre');
+var apellido = document.getElementById('apellido');
+var telefono = document.getElementById('telefono');
+var email = document.getElementById('email');
+var fechaNacimiento = document.getElementById('fechaNacimiento'),
+
+
+    function enviarDatos() {
+        console.log('Enviando datos del usuario');
+
+        return false;
+    }
